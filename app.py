c3717c52140a44e7eff0c2df9521611f35ac7481
Soumyajit Datta <soumyajitdatta123@gmail.com>
2022-10-16 09:24:57 +0530

    proper auth added

--- app.py
+++ app.py
@@ -76,11 +76,7 @@ get:
     message (string): success message
 """
 @app.route("/", methods=["GET"])
-def default():
-  a = auth.authorize(request, APP_SECRET, NONCE, users_collection)
-  if a['error'] == True:
-    return message.message_error(a['code'], a['message'], a['err']) 
-
+def default():  
   return message.message(200, "Welcome to the eFaisla API")
 
 
@@ -107,6 +103,10 @@ get:
 @app.route("/autocomplete", methods=["GET"])
 def autocomplete():
   try:
+    a = auth.authorize(request, APP_SECRET, NONCE, users_collection)
+    if a['error'] == True:
+      return message.message_error(a['code'], a['message'], a['err']) 
+
     limit = -1
     sort = False
 
@@ -175,85 +175,92 @@ post:
 """
 @app.route("/update", methods=["POST"])
 def add_keyword_and_cleantext():
-  spell = False
-  ocr = False
-
   try:
-    ## getting the query parameters
-    id = request.json['id']
-  except:
-    ## returning error if id is not specified
-    return message.message_error(400, "ID is a mandatory field", "Bad Request")
+    a = auth.authorize(request, APP_SECRET, NONCE, users_collection)
+    if a['error'] == True:
+      return message.message_error(a['code'], a['message'], a['err']) 
+
+    spell = False
+    ocr = False
+
+    try:
+      ## getting the query parameters
+      id = request.json['id']
+    except:
+      ## returning error if id is not specified
+      return message.message_error(400, "ID is a mandatory field", "Bad Request")
 
 
-  ## checking if spell check is specified
-  if 'spell' in request.json and request.json['spell'].lower() == 'true':
-    spell = True
+    ## checking if spell check is specified
+    if 'spell' in request.json and request.json['spell'].lower() == 'true':
+      spell = True
 
-  try:
-    ## fetching the document from the database
-    docs = documents_collection.find_one({"_id": ObjectId(id)})['documents']
-  except:
-    return message.message_error(404, "Document not found", "Not Found")
-  
-  try:    
-    clean_t = ""
-    for doc in docs:
-      ## Fetching the document from S3 bucket
-      obj = s3.get_object(Bucket=bucket_name, Key=doc['url'].split("/")[-1])
-
-      ## Reading the fetched document 
-      fs = obj['Body'].read()            
-      pdfReader = PyPDF2.PdfFileReader(io.BytesIO(fs)) 
-
-      ## Checking if the document is readable or not
-      if(len(pdfReader.getPage(0).extractText()) == 0):
-        ## If not readable, performing OCR Detection
-        ocr = True
-        clean_t = clean_t + helper_update.return_string_from_path(fs)
-      else:
-        ## If readable, extracting the text                
-        pdfReader = PyPDF2.PdfFileReader(io.BytesIO(fs)) 
-        for i in range(0,pdfReader.numPages):
-          clean_t = clean_t + pdfReader.getPage(i).extractText()
-  except:
-    ## returning error if the document is not found
-    return message.message_error(500, "Error in reading the file", "Internal Server Error")
+    try:
+      ## fetching the document from the database
+      docs = documents_collection.find_one({"_id": ObjectId(id)})['documents']
+    except:
+      return message.message_error(404, "Document not found", "Not Found")
     
-  
-  ## Correcting the spelling if specified
-  if spell == True:
-    clean_t = helper_update.spell_check(clean_t)
+    try:    
+      clean_t = ""
+      for doc in docs:
+        ## Fetching the document from S3 bucket
+        obj = s3.get_object(Bucket=bucket_name, Key=doc['url'].split("/")[-1])
 
-  ## Extracting Manual Keywords
-  keywords_manual = helper_update.check_manual_keywords(clean_t)
+        ## Reading the fetched document 
+        fs = obj['Body'].read()            
+        pdfReader = PyPDF2.PdfFileReader(io.BytesIO(fs)) 
 
-  ## Removing Symbols + hyphens + stop words + lemmatizing + removing non-english words
-  keyword_corpus = helper_update.distill_string(clean_t)  
+        ## Checking if the document is readable or not
+        if(len(pdfReader.getPage(0).extractText()) == 0):
+          ## If not readable, performing OCR Detection
+          ocr = True
+          clean_t = clean_t + helper_update.return_string_from_path(fs)
+        else:
+          ## If readable, extracting the text                
+          pdfReader = PyPDF2.PdfFileReader(io.BytesIO(fs)) 
+          for i in range(0,pdfReader.numPages):
+            clean_t = clean_t + pdfReader.getPage(i).extractText()
+    except:
+      ## returning error if the document is not found
+      return message.message_error(500, "Error in reading the file", "Internal Server Error")
+      
+    
+    ## Correcting the spelling if specified
+    if spell == True:
+      clean_t = helper_update.spell_check(clean_t)
 
-  ## Extracting the keywords from the corpus
-  key = helper_update.return_keyword(keyword_corpus, 30)
+    ## Extracting Manual Keywords
+    keywords_manual = helper_update.check_manual_keywords(clean_t)
 
-  ## Adding the manual and extracted keywords
-  keys = keywords_manual + key
-  try:
-    ## Updating the document in the database
-    documents_collection.update_one(
-      {"_id": ObjectId(id)}, 
-      {
-        '$set': {"keywords": keys, "cleanText": clean_t}
-      }, 
-      upsert= True
-    )
+    ## Removing Symbols + hyphens + stop words + lemmatizing + removing non-english words
+    keyword_corpus = helper_update.distill_string(clean_t)  
 
-    data = {      
-      "url": docs[0]['url'],
-      "spellCheck": spell,
-      "ocr": ocr,
-      "cleanedText": clean_t,
-      "keywords": keys,      
-    }
-    return message.message_custom(data, 200, "Document updated")    
+    ## Extracting the keywords from the corpus
+    key = helper_update.return_keyword(keyword_corpus, 30)
+
+    ## Adding the manual and extracted keywords
+    keys = keywords_manual + key
+    try:
+      ## Updating the document in the database
+      documents_collection.update_one(
+        {"_id": ObjectId(id)}, 
+        {
+          '$set': {"keywords": keys, "cleanText": clean_t}
+        }, 
+        upsert= True
+      )
+
+      data = {      
+        "url": docs[0]['url'],
+        "spellCheck": spell,
+        "ocr": ocr,
+        "cleanedText": clean_t,
+        "keywords": keys,      
+      }
+      return message.message_custom(data, 200, "Document updated")    
+    except Exception as e:
+      return message.message_error(500, e, "Internal Server Error")
   except Exception as e:
     return message.message_error(500, e, "Internal Server Error")
 
@@ -281,63 +288,69 @@ post:
 """
 @app.route("/search", methods=["POST"])
 def search_keywords():
-  top = 5
-  order_matters = True
-
-  data = request.json
   try:
-    search_key = data["search_key"]  
-  except:
-    return message.message_error(400, "search_key is mandatory field", "Bad Request")
-  
-  if 'top' in data:    
-    top = data["top"]  
-  if 'order_matters' in data and data["order_matters"].lower() == 'false':
-    order_matters = False
+    a = auth.authorize(request, APP_SECRET, NONCE, users_collection)
+    if a['error'] == True:
+      return message.message_error(a['code'], a['message'], a['err']) 
+
+    top = 5
+    order_matters = True
+
+    data = request.json
+    try:
+      search_key = data["search_key"]  
+    except:
+      return message.message_error(400, "search_key is mandatory field", "Bad Request")
     
-  
-  keywords_dataset_cursor = documents_collection.find({"keywords": { '$exists': True} })
-  items = list(keywords_dataset_cursor)
-
-  docs = {}
-  all_docs = {}
-  for i in items:
-    curr_key = str(i['_id'])
-    docs[curr_key] = i['keywords']
-    all_docs[curr_key] = i
-    all_docs[curr_key]["_id"] = str(all_docs[curr_key]["_id"] )
-    for elements in all_docs[curr_key]['documents']:
-      elements["_id"] = str(elements["_id"] )
-
-  ranking = {}
-  for itr in docs.keys() :
-    ranking[itr] = 0
-
-  try:
-    for itr in search_key :
-      if order_matters == True:
-        helper_ranking.make_ranking(docs, itr, search_key.index(itr), ranking)
-      else :
-        helper_ranking.make_ranking(docs, itr, 1, ranking)
-
-    sorted_ranking = helper_ranking.sort_dict(ranking)
-    top_n_ranked_docs = (list(sorted_ranking.keys()))[:top]
-
-    top_n_ranked_final = []
-    for itr in top_n_ranked_docs :      
-      top_n_ranked_final.append(all_docs[itr])
-
-    if len(top_n_ranked_final) == 0:
-      return message.message_error(404, "No documents found", "Not Found")
+    if 'top' in data:    
+      top = data["top"]  
+    if 'order_matters' in data and data["order_matters"].lower() == 'false':
+      order_matters = False
       
-    data = {
-      "docs": top_n_ranked_final,
-      "count": len(top_n_ranked_final)
-    }
-    return message.message_custom(data, 200, "Successefully searched with the keyword")
+    
+    keywords_dataset_cursor = documents_collection.find({"keywords": { '$exists': True} })
+    items = list(keywords_dataset_cursor)
+
+    docs = {}
+    all_docs = {}
+    for i in items:
+      curr_key = str(i['_id'])
+      docs[curr_key] = i['keywords']
+      all_docs[curr_key] = i
+      all_docs[curr_key]["_id"] = str(all_docs[curr_key]["_id"] )
+      for elements in all_docs[curr_key]['documents']:
+        elements["_id"] = str(elements["_id"] )
+
+    ranking = {}
+    for itr in docs.keys() :
+      ranking[itr] = 0
+
+    try:
+      for itr in search_key :
+        if order_matters == True:
+          helper_ranking.make_ranking(docs, itr, search_key.index(itr), ranking)
+        else :
+          helper_ranking.make_ranking(docs, itr, 1, ranking)
+
+      sorted_ranking = helper_ranking.sort_dict(ranking)
+      top_n_ranked_docs = (list(sorted_ranking.keys()))[:top]
+
+      top_n_ranked_final = []
+      for itr in top_n_ranked_docs :      
+        top_n_ranked_final.append(all_docs[itr])
+
+      if len(top_n_ranked_final) == 0:
+        return message.message_error(404, "No documents found", "Not Found")
+        
+      data = {
+        "docs": top_n_ranked_final,
+        "count": len(top_n_ranked_final)
+      }
+      return message.message_custom(data, 200, "Successefully searched with the keyword")
+    except Exception as e:
+      return message.message_error(500, e, "Internal Server Error")
   except Exception as e:
     return message.message_error(500, e, "Internal Server Error")
-
 
 @app.route("/getauthtoken", methods=["POST"])
 def get_auth_token():
